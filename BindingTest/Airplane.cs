dea667c2109f3b7b7d5064945c7837eabeb544b0
Robert Nelson <reg0204@sys-service.de>
2019-05-13 17:20:32 +0200

    class to file airlines + passangers

---
+++ BindingTest/Airplane.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace BindingTest
+{
+    public class Airplane
+    {
+        private static int lastID = 0;
+
+        private int _id;
+        private int _fuelKg;
+        private string _model;
+        private List<Passenger> _passengers = new List<Passenger>();
+
+        public int ID { get { return _id; } }
+        public string Model { get { return _model; } set { _model = value; } }
+        public int FuelLeftKg { get { return _fuelKg; } set { _fuelKg = value; } }
+        public List<Passenger> Passengers { get { return _passengers; } }
+
+        public Airplane()
+        {
+            _id = ++lastID;
+        }
+        public Airplane(string model, int fuelKg)
+        {
+            _id = ++lastID; _model = model; _fuelKg = fuelKg;
+        }
+    }
+}
---
+++ BindingTest/Passenger.cs
@@ -0,0 +1,17 @@
+﻿namespace BindingTest
+{
+    public class Passenger
+    {
+        private static int lastID = 0;
+        private int _id;
+        public string _name;
+
+        public int ID { get { return _id; } }
+        public string Name { get { return _name; } set { _name = value; } }
+        public Passenger(string name)
+        {
+            _id = ++lastID; _name = name;
+        }
+
+    }
+}
